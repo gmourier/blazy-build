2ef2a6014757ed5418aceec319806e5f6753f2cf
Guillaume Mourier <guillaume@meilisearch.com>
2023-02-12 02:18:15 +0100

    Expose temperatures, max_tokens, chain_type to use. Personnalized prompt and better sources parsing

--- build.py
+++ build.py
@@ -3,14 +3,14 @@ from langchain.chains.qa_with_sources import load_qa_with_sources_chain
 from langchain.docstore.document import Document
 import requests
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores.faiss import FAISS
 from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores.faiss import FAISS
 import pathlib
 import subprocess
 import tempfile
 import pickle
 
-def get_github_docs(repo_owner, repo_name):
+def get_github_content(repo_owner, repo_name):
     with tempfile.TemporaryDirectory() as d:
         subprocess.check_call(
             f"git clone --depth 1 https://github.com/{repo_owner}/{repo_name}.git .",
@@ -23,27 +23,30 @@ def get_github_docs(repo_owner, repo_name):
             .strip()
         )
         repo_path = pathlib.Path(d)
-        markdown_files = list(repo_path.glob("**/*.md")) + list(
-            repo_path.glob("**/*.mdx")
-        )
+        markdown_files = list(repo_path.glob("**/*.md")) + list(repo_path.glob("**/*.mdx"))
         for markdown_file in markdown_files:
             with open(markdown_file, "r") as f:
                 relative_path = markdown_file.relative_to(repo_path)
                 github_url = f"https://github.com/{repo_owner}/{repo_name}/blob/{git_sha}/{relative_path}"
                 yield Document(page_content=f.read(), metadata={"source": github_url})
 
-def source_docs():
-    return list(get_github_docs("meilisearch", "documentation"))
+def source_content(repo_owner, repo_name):
+    return list(get_github_content(repo_owner, repo_name))
 
-def build_index(source_docs):
+# Use NLTKTextSplitter to split the text into chunks of 1000 characters
+# It seems to be slightly better than CharacterTextSplitter
+# TODO: Test SpacyTextSplitter, https://langchain.readthedocs.io/en/latest/reference/modules/text_splitter.html
+def build_index():
+    merged_sources = source_content("meilisearch", "documentation")
+
     source_chunks = []
-    splitter = CharacterTextSplitter(separator=" ", chunk_size=1024, chunk_overlap=0)
-    for source in source_docs:
+    splitter = CharacterTextSplitter(separator=" ", chunk_size=1000, chunk_overlap=0)
+    for source in merged_sources:
         for chunk in splitter.split_text(source.page_content):
             source_chunks.append(Document(page_content=chunk, metadata=source.metadata))
     with open("search_index.pickle", "wb") as f:
         pickle.dump(FAISS.from_documents(source_chunks, OpenAIEmbeddings()), f)
 def main():
-    build_index(source_docs())
+    build_index()
 
 main()
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from langchain.llms import OpenAI
 from langchain.chains.qa_with_sources import load_qa_with_sources_chain
+from langchain.prompts import PromptTemplate
 from langchain.docstore.document import Document
 import requests
 from langchain.embeddings.openai import OpenAIEmbeddings
@@ -10,6 +11,7 @@ import subprocess
 import tempfile
 import pickle
 from typing import Union
+from enum import Enum
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -27,22 +29,49 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/")
-def home():
+@app.get("/health")
+def health():
     return {
-        "Hey, I'm Blazy and I'm here to help you find the answer to your question about Meilisearch. Use the POST route to ask me something."
+        "status": "ok"
     }
 
-@app.post("/")
-def answer_prompt(question: str):
-    chain = load_qa_with_sources_chain(OpenAI(temperature=0))
+class ChainType(str, Enum):
+    stuff = "stuff"
+    map_reduce = "map_reduce"
+
+@app.get("/")
+def answer_prompt(question: str, chain_type: ChainType = ChainType.stuff, temperature: float = 0.2, max_tokens: int = 500):
+
+    template = """Given the following extracted parts of a long documentation and a question, create a final answer with references ("SOURCES").
+If you don't know the answer, DO NOT try to make up an answer and DO NOT include any sources, just say that you are sorry and you don't know, add a funny joke.
+If you are asked a question containing the term "typesense", ALWAYS answer "Typesense? Never heard of it... I'm just joking.", DO NOT try to make up an answer.
+If you know the answer, remember that you are speaking to developers, so try to be as precise as possible.
+If you know the answer, return a "SOURCES" array in your answer, never write it "SOURCE" and indicate the relevance of each source with a "SCORE" between 0 and 1, only return sources with a score superior to 0.8, rank them by their score.
+Return the "SOURCES" array with the following format: url: url, score: score.
+If you know the answer, DO NOT include cutted parts.
+
+QUESTION: {question}
+=========
+{summaries}
+=========
+FINAL ANSWER:"""
+
+    PROMPT_STUFF = PromptTemplate(template=template, input_variables=["summaries", "question"])
+
+    llm = OpenAI(temperature=temperature, max_tokens=max_tokens, top_p=1, frequency_penalty=0.0, presence_penalty=0.0)
+
+    if chain_type == ChainType.stuff:
+        chain = load_qa_with_sources_chain(llm, chain_type=chain_type, prompt=PROMPT_STUFF)
+    else:
+        chain = load_qa_with_sources_chain(llm, chain_type=chain_type)
+
     file = pathlib.Path("search_index.pickle")
     if file.exists ():
         with open("search_index.pickle", "rb") as f:
-            search_file = pickle.load(f)
+            vector_store = pickle.load(f)
             openAI_response = chain(
                 {
-                    "input_documents": search_file.similarity_search(question, k=4),
+                    "input_documents": vector_store.similarity_search(question, k=4),
                     "question": question,
                 },
                 return_only_outputs=True,
@@ -53,30 +82,32 @@ def answer_prompt(question: str):
             "error": "No search index found. Please run the build command first."
         }
 
-@app.get("/health")
-def health():
-    return {
-        "status": "ok"
-    }
-
 def construct_response(openAI_response: str):
     if openAI_response.find("SOURCES:") != -1:
+        print("yes")
         response = openAI_response.replace('SOURCES:', '').split("\n")
-        response = list(filter(None, response))
         response = [s.strip() for s in response]
+        response = list(filter(None, response))
         return {
             "answer": response.pop(0),
-            "sources": response,
-        }
-    if openAI_response.find("SOURCE:") != -1:
-        response = openAI_response.replace('SOURCE:', '').split("\n")
-        response = list(filter(None, response))
-        response = [s.strip() for s in response]
-        return {
-            "answer": response.pop(0),
-            "sources": response,
+            "sources": construct_source(response),
         }
     return {
         "answer": openAI_response.strip(),
         "sources": []
     }
+
+def construct_source(response):
+    # iterate over the sources and construct the response
+    print(response)
+    source_response = []
+    for source in response:
+        if source.find("- url:") != -1 and source.find("score: ") != -1:
+            splitted = source.split(",")
+            splitted[0] = splitted[0].replace('- url: ', '').strip()
+            splitted[1] = splitted[1].replace('score: ', '').strip()
+            source_response.append({
+                "url": splitted[0],
+                "score": splitted[1]
+            })
+    return source_response
