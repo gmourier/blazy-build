84bb717a35817d72f216dce063122313ac70a4ba
Guillaume Mourier <guillaume@meilisearch.com>
2023-02-12 11:21:27 +0100

    Add a build endpoint

--- main.py
+++ main.py
@@ -113,3 +113,38 @@ def construct_source(response):
                 "score": splitted[1]
             })
     return source_response
+
+@app.post("/build")
+def build_index():
+    merged_sources = source_content("meilisearch", "documentation")
+
+    source_chunks = []
+    splitter = CharacterTextSplitter(separator=" ", chunk_size=1000, chunk_overlap=0)
+    for source in merged_sources:
+        for chunk in splitter.split_text(source.page_content):
+            source_chunks.append(Document(page_content=chunk, metadata=source.metadata))
+    with open("search_index.pickle", "wb") as f:
+        pickle.dump(FAISS.from_documents(source_chunks, OpenAIEmbeddings()), f)
+
+def source_content(repo_owner, repo_name):
+    return list(get_github_content(repo_owner, repo_name))
+
+def get_github_content(repo_owner, repo_name):
+    with tempfile.TemporaryDirectory() as d:
+        subprocess.check_call(
+            f"git clone --depth 1 https://github.com/{repo_owner}/{repo_name}.git .",
+            cwd=d,
+            shell=True,
+        )
+        git_sha = (
+            subprocess.check_output("git rev-parse HEAD", shell=True, cwd=d)
+            .decode("utf-8")
+            .strip()
+        )
+        repo_path = pathlib.Path(d)
+        markdown_files = list(repo_path.glob("**/*.md")) + list(repo_path.glob("**/*.mdx"))
+        for markdown_file in markdown_files:
+            with open(markdown_file, "r") as f:
+                relative_path = markdown_file.relative_to(repo_path)
+                github_url = f"https://github.com/{repo_owner}/{repo_name}/blob/{git_sha}/{relative_path}"
+                yield Document(page_content=f.read(), metadata={"source": github_url})
