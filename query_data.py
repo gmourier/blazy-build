43cf7f97140f5d4ef1425da544ee581a0b8ea845
Guillaume Mourier <guillaume@meilisearch.com>
2023-02-17 15:32:36 +0100

    Refactor code

--- query_data.py
+++ query_data.py
@@ -1,20 +1,17 @@
-"""Create a ChatVectorDBChain for question/answering."""
+"""Chains builder"""
 from langchain.callbacks.base import AsyncCallbackManager
 from langchain.callbacks.tracers import LangChainTracer
 from langchain.chains import ChatVectorDBChain
-# from langchain.chains.chat_vector_db.prompts import (CONDENSE_QUESTION_PROMPT, QA_PROMPT)
+from langchain.chains import VectorDBQAWithSourcesChain
 from langchain.chains.llm import LLMChain
 from langchain.chains.question_answering import load_qa_chain
+from langchain.chains.qa_with_sources import load_qa_with_sources_chain
 from langchain.llms import OpenAI
 from langchain.vectorstores.base import VectorStore
 
-
 from langchain.prompts.prompt import PromptTemplate
 
-
-
-
-def get_chain(
+def get_chat_chain(
     vectorstore: VectorStore, question_handler, stream_handler, tracing: bool = False
 ) -> ChatVectorDBChain:
     condense_prompt = """Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
@@ -80,10 +77,46 @@ MARKDOWN ANSWER:"""
         streaming_llm, chain_type="stuff", prompt=QA_PROMPT, callback_manager=manager
     )
 
-    qa = ChatVectorDBChain(
+    chat = ChatVectorDBChain(
         vectorstore=vectorstore,
         combine_docs_chain=doc_chain,
         question_generator=question_generator,
         callback_manager=manager,
+    )
+    return chat
+
+def get_qa_chain(
+    vectorstore: VectorStore
+) -> VectorDBQAWithSourcesChain:
+    qa_prompt = """Given the following extracted parts of a long documentation and a question, create a final answer with references ("SOURCES").
+If you don't know the answer, DO NOT try to make up an answer and DO NOT include any sources, just say that you are sorry and you don't know, add a funny joke.
+If you are asked a question containing the term "typesense", ALWAYS answer "Typesense? Never heard of it... I'm just joking.", DO NOT try to make up an answer.
+If you know the answer, remember that you are speaking to developers, so try to be as precise as possible.
+If you know the answer, return a "SOURCES" array in your answer, never write it "SOURCE" and indicate the relevance of each source with a "SCORE" between 0 and 1, only return sources with a score superior to 0.8, rank them by their score.
+Return the "SOURCES" array with the following format: url: url, score: score.
+If you know the answer, DO NOT include cutted parts.
+
+QUESTION: {question}
+=========
+{summaries}
+=========
+FINAL ANSWER:"""
+
+    PROMPT_STUFF = PromptTemplate(template=qa_prompt, input_variables=["summaries", "question"])
+
+    """Create a VectorDBQAWithSourcesChain for question/answering."""
+    llm = OpenAI(
+        temperature=0.2,
+        max_tokens=500,
+        top_p=1,
+        frequency_penalty=0.0,
+        presence_penalty=0.0
+    )
+
+    doc_chain = load_qa_with_sources_chain(llm, chain_type="stuff", prompt=PROMPT_STUFF)
+
+    qa = VectorDBQAWithSourcesChain(
+        vectorstore=vectorstore,
+        combine_documents_chain=doc_chain,
     )
     return qa
